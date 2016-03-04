09e9676933ffcd6b6b0e9792c200975e0cadb282
samtianlinyang <samtianlinyang@gmail.com>
2016-03-03 19:33:31 -0500

    Homework 5 submission

---
+++ Homework 5/ViewExercises/ViewExercises/Exercise5ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  Exercise5ViewController.swift
+//  ViewExercises
+//
+//  Created by William Martin on 1/22/16.
+//  Copyright © 2016 Anomalus. All rights reserved.
+//
+
+import UIKit
+
+class Exercise5ViewController: UIViewController, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows you want to display here.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Produce an instance of UITableViewCell, populate it properly, and return it here.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
