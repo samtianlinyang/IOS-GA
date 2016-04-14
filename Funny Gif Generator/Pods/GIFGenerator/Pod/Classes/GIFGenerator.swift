94780d31eeba8593af776c438dfa38f9ab5356be
samtianlinyang <samtianlinyang@gmail.com>
2016-04-14 19:35:07 -0400

    Final submission

---
+++ Funny Gif Generator/Pods/GIFGenerator/Pod/Classes/GIFGenerator.swift
@@ -0,0 +1,154 @@
+import ImageIO
+import MobileCoreServices
+import AVFoundation
+
+
+@objc public class GifGenerator: NSObject {
+    
+    var cmTimeArray:[NSValue] = []
+    var framesArray:[UIImage] = []
+    
+    /**
+     Generate a GIF using a set of images
+     You can specify the loop count and the delays between the frames.
+     
+     :param: imagesArray an array of images
+     :param: repeatCount the repeat count, defaults to 0 which is infinity
+     :param: frameDelay an delay in seconds between each frame
+     :param: callback set a block that will get called when done, it'll return with data and error, both which can be nil
+     */
+    public func generateGifFromImages(imagesArray imagesArray:[UIImage], repeatCount: Int = 100, frameDelay: NSTimeInterval, destinationURL: NSURL, callback:(data: NSData?, error: NSError?) -> ()) {
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) { () -> Void in
+            
+            if let imageDestination = CGImageDestinationCreateWithURL(destinationURL, kUTTypeGIF, imagesArray.count, nil) {
+                
+                let frameProperties:CFDictionaryRef = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFDelayTime as String: frameDelay]]
+                let gifProperties:CFDictionaryRef = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: repeatCount]]
+                
+                for image in imagesArray {
+                    CGImageDestinationAddImage(imageDestination, image.CGImage!, frameProperties)
+                }
+                
+                CGImageDestinationSetProperties(imageDestination, gifProperties)
+                if CGImageDestinationFinalize(imageDestination) {
+                    
+                    print("animated GIF file created at ", destinationURL)
+                    
+                    do {
+                        if let path = destinationURL.path {
+                            let attr : NSDictionary? = try NSFileManager.defaultManager().attributesOfItemAtPath(path)
+                            
+                            if let _attr = attr {
+                                print("FILE SIZE: ", NSByteCountFormatter.stringFromByteCount(Int64(_attr.fileSize()), countStyle: .File))
+                            }
+                        }
+                    } catch {
+                        print("Error: \(error)")
+                    }
+                    
+                    callback(data: NSData(contentsOfURL: destinationURL), error: nil)
+                } else {
+                    callback(data: nil, error: self.errorFromString("Couldn't create the final image"))
+                }
+            }
+        }
+    }
+    
+    /**
+     Generate a GIF using a set of video file (NSURL)
+     You can specify the loop count and the delays between the frames.
+     
+     :param: videoURL an url where you video file is stored
+     :param: repeatCount the repeat count, defaults to 0 which is infinity
+     :param: frameDelay an delay in seconds between each frame
+     :param: callback set a block that will get called when done, it'll return with data and error, both which can be nil
+     */
+    public func generateGifFromVideoURL(videoURL videoUrl:NSURL, repeatCount: Int = 0, framesInterval:Int, frameDelay: NSTimeInterval, destinationURL: NSURL, callback:(data: NSData?, error: NSError?) -> ()) {
+        
+        self.generateFrames(videoUrl, framesInterval: framesInterval) { (images) -> () in
+            if let images = images {
+                self.generateGifFromImages(imagesArray: images, repeatCount: repeatCount, frameDelay: frameDelay, destinationURL: destinationURL, callback: { (data, error) -> () in
+                    self.cmTimeArray = []
+                    self.framesArray = []
+                    callback(data: data, error: error)
+                })
+            }
+        }
+    }
+    
+    // MARK: THANKS TO: http://stackoverflow.com/questions/4001755/trying-to-understand-cmtime-and-cmtimemake
+   private func generateFrames(url:NSURL, framesInterval:Int, callback:(images:[UIImage]?) -> ()) {
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) { () -> Void in
+            self.generateCMTimesArrayOfFramesUsingAsset(framesInterval, asset: AVURLAsset(URL: url))
+            
+            var i = 0
+            let test:AVAssetImageGeneratorCompletionHandler = { (_, im:CGImage?, _, result:AVAssetImageGeneratorResult, error:NSError?) in
+                if(result == AVAssetImageGeneratorResult.Succeeded) {
+                    print("Succeed")
+                    if let image = im {
+                        self.framesArray.append(UIImage(CGImage: image))
+                    }
+                } else if (result == AVAssetImageGeneratorResult.Failed) {
+                    print("Failed with error")
+                    callback(images: nil);
+                } else if (result == AVAssetImageGeneratorResult.Cancelled) {
+                    print("Canceled")
+                    callback(images: nil);
+                }
+                i++
+                if(i == self.cmTimeArray.count) {
+                    //Thumbnail generation completed
+                    callback(images: self.framesArray)
+                }
+            }
+            let generator = AVAssetImageGenerator(asset: AVAsset(URL: url))
+            generator.apertureMode = AVAssetImageGeneratorApertureModeCleanAperture;
+            generator.appliesPreferredTrackTransform = true;
+            generator.requestedTimeToleranceBefore = kCMTimeZero;
+            generator.requestedTimeToleranceAfter = kCMTimeZero;
+            generator.maximumSize = CGSizeMake(200, 200);
+            
+            generator.generateCGImagesAsynchronouslyForTimes(self.cmTimeArray, completionHandler: test)
+        }
+    }
+    
+   private func generateCMTimesArrayOfAllFramesUsingAsset(asset:AVURLAsset) {
+        if cmTimeArray.count > 0 {
+            cmTimeArray.removeAll()
+        }
+        
+        for (var t:Int64 = 0; t < asset.duration.value; t++) {
+            let thumbTime = CMTimeMake(t, asset.duration.timescale)
+            let value = NSValue(CMTime: thumbTime)
+            cmTimeArray.append(value)
+        }
+    }
+    
+    private func generateCMTimesArrayOfFramesUsingAsset(framesInterval:Int, asset:AVURLAsset) {
+        
+        let videoDuration = Int(ceilf((Float(Int(asset.duration.value)/Int(asset.duration.timescale)))))
+        
+        if cmTimeArray.count > 0 {
+            cmTimeArray.removeAll()
+        }
+        
+        for (var t = 0; t < videoDuration; t++) {
+            let tempInt = Int64(t)
+            let tempCMTime = CMTimeMake(tempInt, asset.duration.timescale)
+            let interval = Int32(framesInterval)
+            
+            for (var j = 1; j < framesInterval+1; j++) {
+                let newCMtime = CMTimeMake(Int64(j), interval)
+                let addition = CMTimeAdd(tempCMTime, newCMtime)
+                cmTimeArray.append(NSValue(CMTime: addition))
+            }
+        }
+    }
+
+    private func errorFromString(string: String, code: Int = -1) -> NSError {
+        let dict = [NSLocalizedDescriptionKey: string]
+        return NSError(domain: "org.cocoapods.GIFGenerator", code: code, userInfo: dict)
+    }
+}
---
+++ Funny Gif Generator/Pods/Regift/README.markdown
@@ -0,0 +1,78 @@
+# Regift
+Easily convert a video to a GIF on iOS and OSX.
+
+[![Version](https://img.shields.io/cocoapods/v/Regift.svg?style=flat)](http://cocoadocs.org/docsets/Regift)
+[![License](https://img.shields.io/cocoapods/l/Regift.svg?style=flat)](http://cocoadocs.org/docsets/Regift)
+[![Platform](https://img.shields.io/cocoapods/p/Regift.svg?style=flat)](http://cocoadocs.org/docsets/Regift)
+[![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
+
+## Installation
+### Cocoapods
+
+Regift is available through [CocoaPods](http://cocoapods.org), and requires Swift 2. To install
+it, simply add the following line to your Podfile:
+
+    pod "Regift"
+
+### Carthage
+
+Regift is available through [Carthage](https://github.com/Carthage/Carthage).
+
+    github 'matthewpalmer/Regift'
+
+## Quick Start
+
+```swift
+import Regift
+```
+
+Synchronous GIF creation:
+
+```swift
+let videoURL   = ...
+let frameCount = 16
+let delayTime  = Float(0.2)
+let loopCount  = 0    // 0 means loop forever
+
+let regift = Regift(sourceFileURL: videoURL, frameCount: frameCount, delayTime: delayTime, loopCount: loopCount)
+print("Gif saved to \(regift.createGif())")
+
+let startTime = Float(30)
+let duration  = Float(15)
+let frameRate = 15
+
+let trimmedRegift = Regift(sourceFileURL: URL, startTime: startTime, duration: duration, frameRate: frameRate, loopCount: loopCount)
+print("Gif saved to \(trimmedRegift.createGif())")
+```
+
+Asynchronous GIF creation:
+
+```swift
+let videoURL   = ...
+let frameCount = 16
+let delayTime  = Float(0.2)
+let loopCount  = 0    // 0 means loop forever
+
+Regift.createGIFFromSource(videoURL, frameCount: frameCount, delayTime: delayTime) { (result) in
+    print("Gif saved to \(result)")
+}
+
+let startTime = Float(30)
+let duration  = Float(15)
+let frameRate = 15
+
+Regift.createGIFFromSource(videoURL, startTime: startTime, duration: duration, frameRate: frameRate) { (result) in
+    print("Gif saved to \(result)")
+}
+```
+
+## Acknowledgements
+Thanks to [Rob Mayoff's Gist](https://gist.github.com/mayoff/4969104), without which this library wouldn't exist.
+
+My personal thanks to all of Regift’s contributors:
+
+* caughtinflux
+* samuelbeek
+* sebyddd
+* nakajijapan
+* dbburgess
